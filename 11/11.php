8fc358174fd39cdd991725b102f2de2444379bc5
Sameera S <sameera1595@gmail.com>
2015-11-25 22:28:32 +0530

    indent fixed

--- 11/11.php
+++ 11/11.php
@@ -14,30 +14,29 @@
 	}
 function insert()
 {
-		$n=$_POST["usrName"];
-		$a1=$_POST["Addr1"];
-		$a2=$_POST["Addr2"];
-		$e=$_POST["email"];
+	$n=$_POST["usrName"];
+	$a1=$_POST["Addr1"];
+	$a2=$_POST["Addr2"];
+	$e=$_POST["email"];
 
-
- 		$q="insert into users values('$n','$a1','$a2','$e')";
- 		mysql_query($q);
+ 	$q="insert into users values('$n','$a1','$a2','$e')";
+ 	mysql_query($q);
  
-	 	$q="select * from users";
-	 	$v=mysql_query($q);
+	$q="select * from users";
+	$v=mysql_query($q);
 
-	 	print "<table border=1>
-		  <tr>
-		   <th>Name</th>
-		   <th>Addr1</th>
-		   <th>Addr2</th>
-		   <th>Email</th>
-		  </tr>";
-	 	while($b=mysql_fetch_row($v))
-	 	{
-			print "<tr><td>$b[0]</td><td>$b[1]</td><td>$b[2]</td><td>$b[3]</td></tr>";
-		}
-		print "</table>";
+	print "<table border=1>
+		<tr>
+		  <th>Name</th>
+		  <th>Addr1</th>
+		  <th>Addr2</th>
+		  <th>Email</th>
+		 </tr>";
+	while($b=mysql_fetch_row($v))
+	{
+		print "<tr><td>$b[0]</td><td>$b[1]</td><td>$b[2]</td><td>$b[3]</td></tr>";
+	}
+	print "</table>";
 }
 
 function search()
